edb412fdba2093d54b4a77085d50fef875272c90
kaiiboraka <kaiiboraka@gmail.com>
2025-10-29 16:37:02 -0600

    trying to fix rooms

--- Scripts/Levels/LevelManager.cs
+++ Scripts/Levels/LevelManager.cs
@@ -3,20 +3,13 @@ using Godot.Collections;
 
 public partial class LevelManager : Node
 {
-    [Export] private Array<NodePath> _roomPaths = new();
-
-    private Array<LevelRoom> _rooms = new();
+    [Export] private Array<LevelRoom> _rooms = new();
 
     public override void _Ready()
     {
-        foreach (var path in _roomPaths)
+        foreach (var room in _rooms)
         {
-            var room = GetNode<LevelRoom>(path);
-            if (room != null)
-            {
-                _rooms.Add(room);
-                room.PlayerEntered += OnPlayerEnteredRoom;
-            }
+            room.PlayerEntered += OnPlayerEnteredRoom;
         }
 
         // Initially, hide all rooms except the first one (presumably the starting room)

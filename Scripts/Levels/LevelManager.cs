d742a26dedd0d72bc1d4eade47a402b15a2470e9
Brandon Graham <kaiiboraka@gmail.com>
2025-11-05 12:36:03 -0700

    Enemy behavior disable optimization

--- Scripts/Levels/LevelManager.cs
+++ Scripts/Levels/LevelManager.cs
@@ -5,31 +5,57 @@ public partial class LevelManager : Node
 {
     [Export] private Array<LevelRoom> _rooms = new();
 
+    private LevelRoom _currentRoom;
+    private LevelRoom _previousRoom;
+
     public override void _Ready()
     {
         foreach (var room in _rooms)
         {
-            room.PlayerEntered += OnPlayerEnteredRoom;
+            room.PlayerEntered += OnPlayerEnteredRoomBoundary;
+            room.PlayerExited += OnPlayerExitedRoomBoundary;
         }
 
         // Initially, hide all rooms except the first one (presumably the starting room)
         if (_rooms.Count > 0)
         {
-            OnPlayerEnteredRoom(_rooms[0]);
+            _currentRoom = _rooms[0];
+            UpdateRoomStates();
         }
     }
+    public void OnPlayerEnteredRoomBoundary(LevelRoom enteredRoom)
+    {
+        // Only update the previous room if we are coming from another room (not a hallway)
+        if (_currentRoom != null)
+        {
+            _previousRoom = _currentRoom;
+        }
+        _currentRoom = enteredRoom;
+        UpdateRoomStates();
+    }
 
-    public void OnPlayerEnteredRoom(LevelRoom enteredRoom)
+    public void OnPlayerExitedRoomBoundary(LevelRoom exitedRoom)
+    {
+        _previousRoom = exitedRoom;
+        _currentRoom = null; // Player is in a hallway
+        UpdateRoomStates();
+    }
+
+    private void UpdateRoomStates()
     {
         foreach (var room in _rooms)
         {
-            if (room == enteredRoom)
+            // A room is active if it's the one the player is currently in,
+            // or the one they just came from.
+            bool shouldBeActive = (room == _currentRoom || room == _previousRoom);
+
+            if (shouldBeActive)
             {
-                room.ShowRoom();
+                room.Activate();
             }
             else
             {
-                room.HideRoom();
+                room.Deactivate();
             }
         }
     }
--- Scripts/Levels/LevelRoom.cs
+++ Scripts/Levels/LevelRoom.cs
@@ -1,5 +1,6 @@
 using Godot;
 using Godot.Collections;
+using System.Collections.Generic;
 
 public partial class LevelRoom : Node3D
 {
@@ -7,9 +8,9 @@ public partial class LevelRoom : Node3D
     [Signal] public delegate void PlayerExitedEventHandler(LevelRoom room);
 
     [Export] private Area3D _triggerVolume;
-    [Export] private Array<Node3D> _otherRooms;
-
     [Export] private Array<EnemySpawner> _spawners;
+
+    private readonly List<Enemy> _enemiesInRoom = new();
 
     public override void _Ready()
     {
@@ -19,7 +20,7 @@ public partial class LevelRoom : Node3D
             _triggerVolume.BodyExited += OnBodyExited;
         }
 
-        _spawners = new Array<EnemySpawner>();
+        _spawners ??= new Array<EnemySpawner>();
         foreach (var child in GetChildren())
         {
             if (child is EnemySpawner spawner)
@@ -27,6 +28,36 @@ public partial class LevelRoom : Node3D
                 _spawners.Add(spawner);
             }
         }
+
+        // Find any enemies that are pre-placed in the room in the editor
+        FindEnemiesRecursively(this);
+    }
+
+    private void FindEnemiesRecursively(Node node)
+    {
+        foreach (var child in node.GetChildren())
+        {
+            if (child is Enemy enemy)
+            {
+                RegisterEnemy(enemy);
+            }
+            else
+            {
+                FindEnemiesRecursively(child);
+            }
+        }
+    }
+
+    public void RegisterEnemy(Enemy enemy)
+    {
+        if (_enemiesInRoom.Contains(enemy)) return;
+        _enemiesInRoom.Add(enemy);
+        enemy.AssociatedRoom = this;
+    }
+
+    public void UnregisterEnemy(Enemy enemy)
+    {
+        _enemiesInRoom.Remove(enemy);
     }
 
     private void OnBodyEntered(Node3D body)
@@ -34,60 +65,47 @@ public partial class LevelRoom : Node3D
         if (body is PlayerBody)
         {
             EmitSignal(SignalName.PlayerEntered, this);
-            // ShowOtherRooms();
-            ShowRoom();
+        }
+        else if (body is Enemy enemy && enemy.AssociatedRoom != this)
+        { 
+            enemy.AssociatedRoom?.UnregisterEnemy(enemy);
+            RegisterEnemy(enemy);
         }
     }
 
-    private void OnBodyExited(Node3D body)
-    {
-        if (body is PlayerBody)
+        private void OnBodyExited(Node3D body)
         {
-            EmitSignal(SignalName.PlayerExited, this);
-            HideRoom();
-            // HideOtherRooms();
-        }
-    }
-
-    private void ShowOtherRooms()
-    {
-        if (!_otherRooms.IsNullOrEmpty())
-        {
-            foreach (Node3D room in _otherRooms)
+            if (body is PlayerBody)
             {
-                room.Visible = true;
+                EmitSignal(SignalName.PlayerExited, this);
+            }
+        }
+    
+        public void Activate()
+        {
+            Visible = true;
+            foreach (var enemySpawner in _spawners)
+            {
+                enemySpawner.IsEnabled = true;
+            }
+    
+            foreach (var enemy in _enemiesInRoom)
+            {
+                enemy.Activate();
+            }
+        }
+    
+        public void Deactivate()
+        {
+            Visible = false;
+            foreach (var enemySpawner in _spawners)
+            {
+                enemySpawner.IsEnabled = false;
+            }
+    
+            foreach (var enemy in _enemiesInRoom)
+            {
+                enemy.Deactivate();
             }
         }
     }
-
-    private void HideOtherRooms()
-    {
-        if (!_otherRooms.IsNullOrEmpty())
-        {
-            foreach (Node3D room in _otherRooms)
-            {
-                room.Visible = false;
-            }
-        }
-    }
-
-    public void ShowRoom()
-    {
-        ToggleRoom(true);
-    }
-
-    public void HideRoom()
-    {
-        ToggleRoom(false);
-    }
-
-    private void ToggleRoom(bool which)
-    {
-        this.Visible = which;
-        // HideOtherRooms();
-        foreach (var enemySpawner in _spawners)
-        {
-            enemySpawner.IsEnabled = which;
-        }
-    }
-}

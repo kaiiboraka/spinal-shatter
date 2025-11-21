fdcd27c7f28be8acd92cc97dbb9096742f702c29
kaiiboraka <kaiiboraka@gmail.com>
2025-11-21 12:04:56 -0700

    Refactor Siphon to be event based instead of Process based

--- Scripts/Components/SiphonComponent.cs
+++ Scripts/Components/SiphonComponent.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using Godot;
-using Godot.Collections;
 
 namespace SpinalShatter;
 
@@ -10,15 +9,17 @@ public partial class SiphonComponent : Node
 	[Export] private Node3D _target;
 	[Export] public AudioStreamPlayer3D AudioStreamPlayer_Siphon { get; private set; }
 
-	private HashSet<Pickup> _attractedPickups = new();
-
-	private bool _siphonPressed;
+	private readonly HashSet<Pickup> _attractedPickups = new();
+	private bool _isSiphoning;
 
 	public bool CanSiphon { get; set; }
 
 	public override void _Ready()
 	{
 		base._Ready();
+		_siphonField.AreaEntered += OnSiphonFieldEntered;
+		_siphonField.AreaExited += OnSiphonFieldExited;
+		
 		_siphonField.Visible = false;
 		_siphonField.Monitoring = false;
 	}
@@ -29,63 +30,69 @@ public partial class SiphonComponent : Node
 
 		if (@event.IsActionPressed("Player_Siphon"))
 		{
-			PlayerBody.Instance.DisallowMeleeAttack();
-			PlayerBody.Instance.DisallowRangedAttack();
-			_siphonField.Monitoring = true;
-			_siphonField.Visible = true;
-			_siphonPressed = true;
-			AttractPickups();
-			while (!AudioStreamPlayer_Siphon.IsPlaying()) AudioStreamPlayer_Siphon.Play(1.54f);
+			StartSiphon();
 		}
 
 		if (@event.IsActionReleased("Player_Siphon"))
 		{
-			PlayerBody.Instance.AllowMeleeAttack();
-			PlayerBody.Instance.AllowRangedAttack();
-			_siphonPressed = false;
-			_siphonField.Visible = false;
-			_siphonField.Monitoring = false;
-			ReleaseAllPickups();
-			AudioStreamPlayer_Siphon.Stop();
+			StopSiphon();
 		}
 	}
 
-	public override void _Process(double delta)
+	private void StartSiphon()
 	{
-		if (_siphonPressed)
-		{
-			AttractPickups();
-		}
-		else
-		{
-			ReleaseAllPickups();
-		}
-	}
-
-	private void AttractPickups()
-	{
-		if (_siphonField == null) return;
-
+		PlayerBody.Instance.DisallowMeleeAttack();
+		PlayerBody.Instance.DisallowRangedAttack();
+		_siphonField.Monitoring = true;
+		_siphonField.Visible = true;
+		_isSiphoning = true;
+		
+		// Attract any pickups already inside the field
 		foreach (var area in _siphonField.GetOverlappingAreas())
 		{
-			if (area.GetOwner() is Pickup pickup)
-			{
-				pickup.RemoveCollisionMask3D(LayerNames.PHYSICS_3D.SOLID_GROUND_NUM);
-				pickup.RemoveCollisionMask3D(LayerNames.PHYSICS_3D.SOLID_WALL_NUM);
-				pickup.RemoveCollisionMask3D(LayerNames.PHYSICS_3D.PLATFORM_NUM);
-
-				// Attract the pickup if it's not already being attracted
-				if (_attractedPickups.Add(pickup) &&
-					pickup.State != Pickup.PickupState.Collected &&
-					pickup.State != Pickup.PickupState.Expired)
-				{
-					// GD.Print($"{Time.GetTicksMsec()}: SiphonComponent: Attracting pickup {pickup.Name}, current state: {pickup.State}");
-					pickup.BeginAttraction(_target);
-				}
-
-				pickup.Released += OnPickupReleased;
-			}
+			OnSiphonFieldEntered(area);
 		}
+
+		if (!AudioStreamPlayer_Siphon.IsPlaying())
+		{
+			AudioStreamPlayer_Siphon.Play(1.54f);
+		}
+	}
+
+	private void StopSiphon()
+	{
+		PlayerBody.Instance.AllowMeleeAttack();
+		PlayerBody.Instance.AllowRangedAttack();
+		_isSiphoning = false;
+		_siphonField.Visible = false;
+		_siphonField.Monitoring = false;
+		ReleaseAllPickups();
+		AudioStreamPlayer_Siphon.Stop();
+	}
+
+	private void OnSiphonFieldEntered(Area3D area)
+	{
+		if (!_isSiphoning || area.Owner is not Pickup pickup) return;
+		
+		// Attract the pickup if it's not already being attracted
+		if (_attractedPickups.Add(pickup) &&
+			pickup.State is not (Pickup.PickupState.Collected or Pickup.PickupState.Expired))
+		{
+			pickup.RemoveCollisionMask3D(LayerNames.PHYSICS_3D.SOLID_GROUND_NUM);
+			pickup.RemoveCollisionMask3D(LayerNames.PHYSICS_3D.SOLID_WALL_NUM);
+			pickup.RemoveCollisionMask3D(LayerNames.PHYSICS_3D.PLATFORM_NUM);
+			
+			pickup.BeginAttraction(_target);
+			pickup.Released += OnPickupReleased;
+		}
+	}
+	
+	private void OnSiphonFieldExited(Area3D area)
+	{
+		if (area.Owner is not Pickup pickup || !_attractedPickups.Contains(pickup)) return;
+
+		ReleasePickup(pickup);
+		_attractedPickups.Remove(pickup);
 	}
 
 	private void ReleaseAllPickups()
@@ -94,22 +101,29 @@ public partial class SiphonComponent : Node
 
 		foreach (var pickup in _attractedPickups)
 		{
-			pickup.AddCollisionMask3D(LayerNames.PHYSICS_3D.SOLID_GROUND_NUM);
-			pickup.AddCollisionMask3D(LayerNames.PHYSICS_3D.PLATFORM_NUM);
-			pickup.AddCollisionMask3D(LayerNames.PHYSICS_3D.SOLID_WALL_NUM);
-
-			if (IsInstanceValid(pickup) && pickup.State == Pickup.PickupState.Attracted)
-			{
-				pickup.OnSiphonRelease();
-			}
+			ReleasePickup(pickup);
 		}
-
 		_attractedPickups.Clear();
+	}
+
+	private void ReleasePickup(Pickup pickup)
+	{
+		if (!IsInstanceValid(pickup)) return;
+		
+		pickup.AddCollisionMask3D(LayerNames.PHYSICS_3D.SOLID_GROUND_NUM);
+		pickup.AddCollisionMask3D(LayerNames.PHYSICS_3D.PLATFORM_NUM);
+		pickup.AddCollisionMask3D(LayerNames.PHYSICS_3D.SOLID_WALL_NUM);
+
+		if (pickup.State == Pickup.PickupState.Attracted)
+		{
+			pickup.OnSiphonRelease();
+		}
+		
+		pickup.Released -= OnPickupReleased;
 	}
 
 	private void OnPickupReleased(Pickup pickup)
 	{
-		// GD.Print($"{Time.GetTicksMsec()}: SiphonComponent: Removing pickup {pickup.Name} from attracted set.");
 		_attractedPickups.Remove(pickup);
 	}
 }
